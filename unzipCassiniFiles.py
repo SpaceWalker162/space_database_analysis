0a8ac3952c853e2e00c28146828cdd9180f2be3d
SpaceWalker162 <hgjd162@outlook.com>
2023-06-13 12:22:39 +0800

    update

--- unzipCassiniFiles.py
+++ unzipCassiniFiles.py
@@ -3,11 +3,22 @@ import zipfile
 import sys
 
 path = sys.argv[1]
+print(path)
 files = os.listdir(path)
+lostFiles = []
+lostFilesLogFileName = 'lost.log'
 for file in files:
     filePathAbs = os.path.join(path, file)
     baseName, ext = os.path.splitext(file)
-    if ext.lower() == 'zip':
-        fzip = zipfile.ZipFile(filePathAbs)
-        dst = os.path.join(path, baseName)
-        fzip.extractall(dst)
+    if ext.lower() == '.zip':
+        if zipfile.is_zipfile(filePathAbs):
+            print(file)
+            fzip = zipfile.ZipFile(filePathAbs)
+            dst = os.path.join(path, baseName)
+            fzip.extractall(dst)
+            print(dst)
+        else:
+            lostFiles.append(file)
+with open(lostFilesLogFileName, 'w') as f:
+    print(*lostFiles, sep=',', file=f)
+print("end")
