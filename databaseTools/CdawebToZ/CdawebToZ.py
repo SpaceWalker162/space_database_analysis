e9fd7ab70730e5612e333a4a07555d7ad5cd7412
SpaceWalker162 <hgjd162@outlook.com>
2024-06-19 13:16:34 -0400

    update

--- databaseTools/CdawebToZ/CdawebToZ.py
+++ databaseTools/CdawebToZ/CdawebToZ.py
@@ -9,11 +9,14 @@ if __name__ == '__main__':
     srcdata = os.path.expanduser(srcdata)
     dstdata = os.path.expanduser(dstdata)
 
-    srcdataTree = dbt.readFileInfoRecursively(path=srcdata, verbose=False, facts='stats')
+    verbose = True
+    srcdataTree = dbt.readFileInfoRecursively(path=srcdata, verbose=verbose, facts='stats')
     toD = srcdataTree.toList()
     for src_name_in_list in toD:
         src = os.path.join(srcdata, *src_name_in_list[:-2])
-        dst = os.path.splitext(os.path.join(dstdata, src_name_in_list[:-2]))[0] + '.z.cdf'
+        dst = os.path.splitext(os.path.join(dstdata, *src_name_in_list[:-2]))[0] + '.z.cdf'
 #        if src_name_in_list[0] == 'themis' and 'fgm' in src_name_in_list:
+        if verbose:
+            print('converting ', src)
         CTZ.CdawebTHEMISFile.convert_from_cdaweb_to_z(src, dst)
 
