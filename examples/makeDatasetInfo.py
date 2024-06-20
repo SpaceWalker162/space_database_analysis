09696b6807de2df29aa051dd67024307c613a491
SpaceWalker162 <hgjd162@outlook.com>
2024-06-20 15:10:36 +0800

    update

--- examples/makeDatasetInfo.py
+++ examples/makeDatasetInfo.py
@@ -6,25 +6,25 @@ if __name__ == '__main__':
     database = dbt.Database([databasePath])
 
     ## to initialize uncomment the following line and run
-    database.make_additional_datasets_info()
+#    database.make_additional_datasets_info()
     
     ## to add user defined information to datasets in the database use the following codes
-#    add_dic = {}
-#    for spacecraftInd in range(1, 5):
-#        spacecraftName = 'C' + str(spacecraftInd)
-#
-#        datasetID = spacecraftName + '_CP_FGM_FULL'
-#        dataset = {}
-#        dataset_path = os.path.join('cluster', spacecraftName.lower(), 'fgm', 'mfield_3dvect_fullreso')
-#        dataset.update({'dataset_path': dataset_path})
-#        add_dic[datasetID] = dataset
-#
-#        datasetID = spacecraftName + '_CP_FGM_5VPS'
-#        dataset = {}
-#        dataset_path = os.path.join('cluster', spacecraftName.lower(), 'fgm', 'mfield_3dvect_5vectpersec')
-#        dataset.update({'dataset_path': dataset_path})
-#        add_dic[datasetID] = dataset
-#
-#    database.load_additional_datasets_info()
-#    database.update_additional_datasets_info(add_dic)
-#    database.save_additinal_datasets_info()
+    add_dic = {}
+    for spacecraftInd in range(1, 5):
+        spacecraftName = 'C' + str(spacecraftInd)
+
+        datasetID = spacecraftName + '_CP_FGM_FULL'
+        dataset = {'Id': datasetID}
+        dataset_path = os.path.join('cluster', spacecraftName.lower(), 'fgm', 'mfield_3dvect_fullreso')
+        dataset.update({'dataset_path': dataset_path})
+        add_dic[datasetID] = dataset
+
+        datasetID = spacecraftName + '_CP_FGM_5VPS'
+        dataset = {'Id': datasetID}
+        dataset_path = os.path.join('cluster', spacecraftName.lower(), 'fgm', 'mfield_3dvect_5vectpersec')
+        dataset.update({'dataset_path': dataset_path})
+        add_dic[datasetID] = dataset
+
+    database.load_additional_datasets_info()
+    database.update_additional_datasets_info(add_dic)
+    database.save_additinal_datasets_info()
